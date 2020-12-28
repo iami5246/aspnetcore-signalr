1abf7e8444c9efa09576cfe65fe729288a6b6bb6
iami5246 <iami5246@naver.com>
2020-12-28 18:10:16 +0900

    초기 커밋.

---
+++ EddyWebApplication/wwwroot/js/chat.js
@@ -0,0 +1,47 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.on("GroupJoined", function (message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg =  msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+
+document.getElementById("joinGroup").addEventListener("click", function (event) {
+    var group = document.getElementById("group").value;
+    var user = document.getElementById("userInput").value;
+    connection.invoke("JoinGroup", group, user).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
